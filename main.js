3368fd7235fcb35138883e120e9f969d3a05da7d
pdoongarwal <pdoongarwal@gmail.com>
2015-12-27 19:13:29 +0530

    Updated main.js

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
+var x;
+var y;
+var z=0;
+var str;
+var code;
 $(document).ready(function(){
   	$(document).mousemove(function(e){
 	    //$('#spnCursor').html("X Axis : " + event.pageX + " Y Axis : " + event.pageY);
-	    var x = event.pageX/2;
-	    var y = event.pageY/2;
-	    var z = x + y;
-	    var str = "rgb(" + x + ", " + y + ", " + z + ")"
-	    console.log(str);
+	    x = event.pageX/4;
+	    y = event.pageY/2;
+	    update();
+	});
+
+  	$(document).keydown(function() {
+  		code = event.keyCode || event.which;
+ 		if(code === 38 && z<251) { //Enter keycode
+   			z=z+5;
+ 		}
+ 		if(code === 39 && z<255) { //Enter keycode
+   			z++;
+ 		}
+ 		if(code === 40 && z>4) { //Enter keycode
+   			z=z-5;
+ 		}
+ 		if(code === 37 && z>0) { //Enter keycode
+   			z--;
+ 		}
+  		update();
+	});
+
+	var update = function() {
+		str = "rgb(" + x + ", " + y + ", " + z + ")"
 	    $("body").css("background-color", str);
 	    $('#spnCursor').html(str);
-	  });
+	}
 });
