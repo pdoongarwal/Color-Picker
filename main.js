269687ebb3811dcc8c856fd26ee7dc42dfaa9fe5
sharath chandra velpula <sharath chandra velpula>
2015-12-27 22:39:28 +0530

    Improved

--- main.js
+++ main.js
@@ -3,36 +3,44 @@ var y;
 var z=0;
 var str;
 var code;
+
 $(document).ready(function(){
-  	$(document).mousemove(function(e){
-	 	X_ratio = ($(window).width()-50)/255;
-	 	Y_ratio = ($(window).height()-50)/255;
-	    x = Math.round(event.pageX/X_ratio);
-	    y = Math.round(event.pageY/Y_ratio);
 
-	    update();
-	});
+	var extra = function() {
+	  	$(document).mousemove(function(e){
+		 	X_ratio = ($(window).width()-50)/255;
+		 	Y_ratio = ($(window).height()-50)/255;
+		    x = Math.round(event.pageX/X_ratio);
+		    y = Math.round(event.pageY/Y_ratio);
 
-  	$(document).keydown(function() {
-  		code = event.keyCode || event.which;
- 		if(code === 38 && z<251) { //Enter keycode
-   			z=z+5;
- 		}
- 		if(code === 39 && z<255) { //Enter keycode
-   			z++;
- 		}
- 		if(code === 40 && z>4) { //Enter keycode
-   			z=z-5;
- 		}
- 		if(code === 37 && z>0) { //Enter keycode
-   			z--;
- 		}
-  		update();
-	});
+		    update();
+		});
 
-	var update = function() {
-		str = "rgb(" + x + ", " + y + ", " + z + ")"
-	    $("body").css("background-color", str);
-	    $('#spnCursor').html(str);
-	}
+	  	$(document).keydown(function() {
+	  		code = event.keyCode || event.which;
+	 		if(code === 38 && z<251) { //Enter keycode
+	   			z=z+5;
+	 		}
+	 		if(code === 39 && z<255) { //Enter keycode
+	   			z++;
+	 		}
+	 		if(code === 40 && z>4) { //Enter keycode
+	   			z=z-5;
+	 		}
+	 		if(code === 37 && z>0) { //Enter keycode
+	   			z--;
+	 		}
+	  		update();
+		});
+
+		var update = function() {
+			str = "rgb(" + x + ", " + y + ", " + z + ")";
+	    	$("body").css("background-color", str);
+	    	$('#spnCursor').html(str);
+		}
+
+		return;
+  	}
+
+  	extra();
 });
