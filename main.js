9a795d441cce79662406a8010cd2794f0d4e2198
sharath chandra velpula <sharath chandra velpula>
2015-12-30 15:25:51 +0530

    Completed

--- main.js
+++ main.js
@@ -3,25 +3,86 @@ var y;
 var z=0;
 var str;
 var code;
-
+var ctrlkey = -1;
+var size = 16;
 var extra = function(clicked_id) {
 	id = "#" + clicked_id;
-	$(document).mousemove(function(e){
+	console.log(1);
+	$(document).keydown(function(){
+	 	if(event.keyCode == "66"){
+	 		bgcolor(clicked_id);
+	 	}
+	 	if(event.keyCode == "70"){
+	 		bgFont(clicked_id);
+	 	}
+	 	
+	 	if(event.keyCode == "49") {
+                document.getElementById(clicked_id).style.fontFamily= "monospace";
+        }
+        if(event.keyCode == "50") {
+                document.getElementById(clicked_id).style.fontFamily= "arial";
+        }
+        if(event.keyCode == "51") {
+                document.getElementById(clicked_id).style.fontFamily= "courier";
+        }
+        if(event.keyCode == "52") {
+                document.getElementById(clicked_id).style.fontFamily= "cursive";
+        }
+        if(event.keyCode == "53") {
+                document.getElementById(clicked_id).style.fontFamily= "fantasy";
+        }
+        if(event.keyCode == "54") {
+                document.getElementById(clicked_id).style.fontFamily= "serif";
+        }
+        if(event.keyCode == "55") {
+                document.getElementById(clicked_id).style.fontFamily= "sans-serif";
+        }
+        if(event.keyCode == "56") {
+                document.getElementById(clicked_id).style.fontFamily= "times";
+        }
+        if(event.keyCode == "57") {
+                document.getElementById(clicked_id).style.fontFamily= "tahoma";
+        }
+
+        if(event.keyCode == "109"){
+        		size = size - 1;
+        		 document.getElementById(clicked_id).style.fontSize=size.toString();
+	 	}
+	 	if(event.keyCode == "107") {
+        		size = size + 1;
+        		 document.getElementById(clicked_id).style.fontSize=size.toString();
+	 	}
+
+	  	
+	});
+}
+var bgcolor = function(id){
+	console.log(2);
+	color(id);
+
+
+
+}
+var color = function(id){
+	console.log(3);
+	$(document).mousemove(function(e)
+	{
 		X_ratio = ($(window).width()-50)/255;
 		Y_ratio = ($(window).height()-50)/255;
 		x = Math.round(event.pageX/X_ratio);
 		y = Math.round(event.pageY/Y_ratio);
 
-		update();
+		changeBg(id);
 	});
 
-	$(document).keydown(function() {
+	$(document).keydown(function() 
+	{
 	  	code = event.keyCode || event.which;
 	 	if(code === 38 && z<251) { //Enter keycode
 	   		z=z+5;
 	 	}
 	 	if(code === 39 && z<255) { //Enter keycode
-	   		z++;
+	   		z++
 	 	}
 	 	if(code === 40 && z>4) { //Enter keycode
 	   		z=z-5;
@@ -32,47 +93,69 @@ var extra = function(clicked_id) {
 	 	if(code === 13) {
 	 		$(document).off("mousemove");
 	 		$(document).off("keydown");
-	 	}
-	 	
-	 	if(event.keyCode == "49") {
-                document.getElementById(id).style.fontFamily= "monospace";
-        }
-        if(event.keyCode == "50") {
-                document.getElementById(id).style.fontFamily= "arial";
-        }
-        if(event.keyCode == "51") {
-                document.getElementById(id).style.fontFamily= "courier";
-        }
-        if(event.keyCode == "52") {
-                document.getElementById(id).style.fontFamily= "cursive";
-        }
-        if(event.keyCode == "53") {
-                document.getElementById(id).style.fontFamily= "fantasy";
-        }
-        if(event.keyCode == "54") {
-                document.getElementById(id).style.fontFamily= "serif";
-        }
-        if(event.keyCode == "55") {
-                document.getElementById(id).style.fontFamily= "sans-serif";
-        }
-        if(event.keyCode == "56") {
-                document.getElementById(id).style.fontFamily= "times";
-        }
-        if(event.keyCode == "57") {
-                document.getElementById(id).style.fontFamily= "tahoma";
-        }
-	 		
-	  	update();
+	 	}	
+	 	changeBg(id);
+	});
+	var changeBg = function(id){
+	str = "rgb(" + x + ", " + y + ", " + z + ")"
+	 document.getElementById(id).style.backgroundColor=str;
+	$('#spnCursor').html(str);
+}
+}
+var bgFont = function(id){
+		$(document).mousemove(function(e)
+	{
+		X_ratio = ($(window).width()-50)/255;
+		Y_ratio = ($(window).height()-50)/255;
+		x = Math.round(event.pageX/X_ratio);
+		y = Math.round(event.pageY/Y_ratio);
+
+		changeFc(id);
 	});
 
-	var update = function() {
-		str = "rgb(" + x + ", " + y + ", " + z + ")";
-	    $(id).css("background-color", str);
-	    $('#spnCursor').html(str);
-	}
+	$(document).keydown(function() 
+	{
+	  	code = event.keyCode || event.which;
+	 	if(code === 38 && z<251) { //Enter keycode
+	   		z=z+5;
+	 	}
+	 	if(code === 39 && z<255) { //Enter keycode
+	   		z++
+	 	}
+	 	if(code === 40 && z>4) { //Enter keycode
+	   		z=z-5;
+	 	}
+	 	if(code === 37 && z>0) { //Enter keycode
+	   		z--;
+	 	}
+	 	if(code === 13) {
+	 		$(document).off("mousemove");
+	 		$(document).off("keydown");
+	 	}	
+	 	changeFc(id);
+	});
+	var changeFc = function(id){
+	str = "rgb(" + x + ", " + y + ", " + z + ")"
+	 document.getElementById(id).style.color=str;
+	$('#spnCursor').html(str);
+}
 }
 
-$(document).dblclick(function(){
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
